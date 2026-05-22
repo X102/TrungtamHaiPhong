ef2d6f3aa201831f29ef43ae37b13351221e3507
lopmaybay <67662665+X102@users.noreply.github.com>
2026-05-22 14:10:36 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,113 +1,17 @@
 const mapConfig = {
     "title": "OBIA Land Cover Classification",
-    "author": "Researcher",
-    "contact": "contact@example.com",
+    "author": "Pham Dang Hien",
+    "contact": "lopmaybay@gmail.com",
     "colors": {
-        "Baren1987": "#d285b9",
-        "Building": "#eae9f7",
-        "Ao": "#799108",
-        "Nha": "#985a16",
-        "Cang": "#622e39",
-        "Nuoc_chung": "#90f117",
-        "Thucvat_chung": "#f78abb",
-        "Nha May": "#8a4db8",
-        "Ruong": "#fb25fe",
-        "Agriculture1987": "#4c4c32",
-        "City1987": "#433aed",
-        "Fabric": "#2ec250",
-        "Forest1987": "#236ece",
-        "Forest": "#054d21",
-        "Lake1987": "#8ec8db",
-        "Ruong1987": "#f2ce93",
-        "water1987": "#7c67da",
-        "Port": "#1a912d",
-        "Water": "#9e0e41"
+        "Building": "#4CAF50",
+        "Vegetation": "#2196F3",
+        "Water": "#F44336"
     },
     "layers": [
         {
-            "name": "Baren-1987-E015-C05-GE005",
+            "name": "LULC-2025-RF-3Class",
             "file": "data/layer_0.geojson",
-            "field": "Class"
-        },
-        {
-            "name": "Building-2020-E025-C015-GE02-RD3",
-            "file": "data/layer_1.geojson",
-            "field": "Class"
-        },
-        {
-            "name": "B\u1ea3n \u0111\u1ed3 (Trich_Xuat) - 7",
-            "file": "data/layer_2.geojson",
             "field": "Class_Name"
-        },
-        {
-            "name": "Agriculture-1987_11_E0.19_C0.5_GE0",
-            "file": "data/layer_3.geojson",
-            "field": "Class"
-        },
-        {
-            "name": "City-1987-E033-C1",
-            "file": "data/layer_4.geojson",
-            "field": "Class"
-        },
-        {
-            "name": "Fabric-2020-E045-C1-GE02-RD7",
-            "file": "data/layer_5.geojson",
-            "field": "Class"
-        },
-        {
-            "name": "Forest-1987-E0115-C001-GE0001-RD3",
-            "file": "data/layer_6.geojson",
-            "field": "Class"
-        },
-        {
-            "name": "Forest-2020-E03-C01-GE02-RD3",
-            "file": "data/layer_7.geojson",
-            "field": "Class"
-        },
-        {
-            "name": "Forest-2025-E025-C01-GE02-RD6",
-            "file": "data/layer_8.geojson",
-            "field": "Class"
-        },
-        {
-            "name": "Lake-1987-E035-C05-GE001",
-            "file": "data/layer_9.geojson",
-            "field": "Class"
-        },
-        {
-            "name": "Merged_Final_Result",
-            "file": "data/layer_10.geojson",
-            "field": "Class"
-        },
-        {
-            "name": "Port-2020-E015C005GE02RD3",
-            "file": "data/layer_11.geojson",
-            "field": "Class"
-        },
-        {
-            "name": "Port-2025-E015C005GE02RD6",
-            "file": "data/layer_12.geojson",
-            "field": "Class"
-        },
-        {
-            "name": "Ruong-1987-E04-C05-GE005",
-            "file": "data/layer_13.geojson",
-            "field": "Class"
-        },
-        {
-            "name": "Water-1987-E013-C005-GE01-RD3",
-            "file": "data/layer_14.geojson",
-            "field": "Class"
-        },
-        {
-            "name": "Water-2020-E02C01GE02RD3",
-            "file": "data/layer_15.geojson",
-            "field": "Class"
-        },
-        {
-            "name": "Water-2025-E02C01GE02RD4",
-            "file": "data/layer_16.geojson",
-            "field": "Class"
         }
     ]
 };
